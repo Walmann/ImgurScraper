1c867eec223bba174298c45dc058493fff051a3e
Tov Are Walmann <tovare@walmann.it>
2023-05-03 13:27:43 +0200

    Self-Update script.

--- Start.py
+++ Start.py
@@ -2,7 +2,6 @@ import os
 import sys
 import git
 from github import Github
-from configparser import ConfigParser
 
 # Define the repository information
 REPO_OWNER = "Walmann"
@@ -20,14 +19,9 @@ latest_commit_sha = repo.get_commits()[0].sha
 # Get the current commit SHA
 current_commit_sha = git.Repo(search_parent_directories=True).head.object.hexsha
 
-# Check if there are changes in the settings.ini file
-config = ConfigParser()
-config.read("settings.ini")
-local_settings_sha = git.Repo(search_parent_directories=True).git.hash_object("settings.ini")
-
-# If the latest commit SHA or the settings.ini SHA is different from the current commit SHA
-if latest_commit_sha != current_commit_sha or local_settings_sha != config.get("DEFAULT", "sha"):
-    print("Updating repository and settings.ini...")
+# If the latest commit SHA is different from the current commit SHA
+if latest_commit_sha != current_commit_sha:
+    print("Updating repository...")
 
     # Pull the latest changes from the repository
     repo_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,13 +29,5 @@ if latest_commit_sha != current_commit_sha or local_settings_sha != config.get("
     origin = repo.remote(name="origin")
     origin.pull()
 
-    # Merge changes in the settings.ini file
-    config.read("settings.ini")
-    config.set("DEFAULT", "sha", local_settings_sha)
-    with open("settings.ini", "w") as configfile:
-        config.write(configfile)
-
-    # Restart the script
-    os.execv(sys.executable, [sys.executable] + sys.argv)
-else:
-    print("Repository and settings.ini are up-to-date.")
+# Run the ImgurDownloader script
+os.system("python3 ImgurDownloader.py")
